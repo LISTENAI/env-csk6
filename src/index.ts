e0243189751bb6b8df07c970a301c577aeb76ed4
xychen <xychen@listenai.com>
2021-10-28 10:56:59 +0800

    Migrate to lisa-binary

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,16 @@
-const TOOLS = [
-  'gcc-arm-none-eabi-9',
-];
+import gcc from '@binary/gcc-arm-none-eabi-9';
 
-interface ITool {
-  HOME: string;
-  PATH: string;
-  version(): Promise<string>;
-}
+interface Bundle {
+  bundle: string[];
+  env: Record<string, string>;
+};
 
-export async function path(): Promise<Record<string, string>> {
-  const path: Record<string, string> = {};
-  for (const name of TOOLS) {
-    const mod = await import(`@tool/${name}`) as ITool;
-    path[name] = mod.PATH;
-  }
-  return path;
-}
-
-export async function version(): Promise<Record<string, string>> {
-  const version: Record<string, string> = {};
-  for (const name of TOOLS) {
-    const mod = await import(`@tool/${name}`) as ITool;
-    version[name] = await mod.version();
-  }
-  return version;
-}
-
-export async function env(): Promise<Record<string, string>> {
-  const gcc = await import('@tool/gcc-arm-none-eabi-9');
-  return {
+export default <Bundle>{
+  bundle: [
+    'gcc-arm-none-eabi-9',
+  ],
+  env: {
     ZEPHYR_TOOLCHAIN_VARIANT: 'gnuarmemb',
-    GNUARMEMB_TOOLCHAIN_PATH: gcc.HOME,
-  };
-}
+    GNUARMEMB_TOOLCHAIN_PATH: gcc.homeDir,
+  },
+};
