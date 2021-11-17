874b2fa3a3bafb59d78614261bb83004de12b564
xychen <xychen@listenai.com>
2021-11-17 14:50:04 +0800

    Add flasher

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
-import { Bundle } from '@lisa-env/type';
+import { Bundle, FlasherArgs } from '@lisa-env/type';
 import gcc from '@binary/gcc-arm-none-eabi-9';
+import { join, resolve } from 'path';
+
+const JLINK_DIR = join(__dirname, '..', 'jlink');
+const VENUS_FLASH_BASE = 0x18000000;
+
+function makeFlashExecArgs(partitions: Record<number, string>, otherArgs: string[] = []): FlasherArgs {
+  const execArgs = [
+    `-jflashlog${resolve(join(JLINK_DIR, 'jflash.log'))}`,
+    `-jlinkdevicesxmlpath${resolve(join(JLINK_DIR, 'JLinkDevices.xml'))}`,
+    `-openprj${resolve(join(JLINK_DIR, 'Venus.jflash'))}`,
+  ];
+
+  for (const [key, file] of Object.entries(partitions)) {
+    const addr = parseInt(key);
+    if (!isNaN(addr)) {
+      execArgs.push(`-open${resolve(file)},0x${(VENUS_FLASH_BASE + addr).toString(16)}`)
+    }
+  }
+
+  return {
+    command: 'JFlash',
+    args: [...execArgs, ...otherArgs, '-auto', '-exit'],
+  };
+}
 
 export default <Bundle>{
   binaries: [
@@ -10,4 +34,5 @@ export default <Bundle>{
     ZEPHYR_TOOLCHAIN_VARIANT: 'gnuarmemb',
     GNUARMEMB_TOOLCHAIN_PATH: gcc.homeDir,
   },
+  flasher: { makeFlashExecArgs },
 };
