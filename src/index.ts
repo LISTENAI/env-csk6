bccaa6701b9641af7d0668aac01edf40c5afdc68
jw <105700413+listenai-jw@users.noreply.github.com>
2023-08-30 17:04:39 +0800

    use seperate sdk package

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import { Bundle } from '@lisa-env/type';
 import gcc from '@binary/gcc-arm-none-eabi-10.3';
-import zephyrSDK from '@binary/zephyr-sdk-multi';
+import zephyrSDK_14 from '@binary/zephyr-sdk-0.14.2';
+import zephyrSDK_16 from '@binary/zephyr-sdk-0.16.1';
 
 export default <Bundle>{
   binaries: [
     'cskburn',
-    'zephyr-sdk-multi',
+    'zephyr-sdk-0.14.2',
+    'zephyr-sdk-0.16.1',
     'gcc-arm-none-eabi-10.3',
     'jlink-venus',
   ],
   env: {
     ZEPHYR_TOOLCHAIN_VARIANT: 'zephyr',
-    ZEPHYR_SDK_INSTALL_DIR: zephyrSDK.homeDir,
+    ZEPHYR_14_SDK_INSTALL_DIR: zephyrSDK_14.homeDir,
+    ZEPHYR_16_SDK_INSTALL_DIR: zephyrSDK_16.homeDir,
     GNUARMEMB_TOOLCHAIN_PATH: gcc.homeDir,
   },
 };
