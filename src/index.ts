b2d399825e7c2a07171ced2c04f2741bdda6cd99
jw <105700413+listenai-jw@users.noreply.github.com>
2023-08-30 12:11:53 +0800

    add multi-sdk pack

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
 import { Bundle } from '@lisa-env/type';
 import gcc from '@binary/gcc-arm-none-eabi-10.3';
-import zephyrSDK_14 from '@binary/zephyr-sdk-0.14.2';
-import zephyrSDK_16 from '@binary/zephyr-sdk-0.16.1';
+import zephyrSDK from '@binary/zephyr-sdk-multi';
 
 export default <Bundle>{
   binaries: [
     'cskburn',
-    'zephyr-sdk-0.14.2',
-    'zephyr-sdk-0.16.1',
+    'zephyr-sdk-multi',
     'gcc-arm-none-eabi-10.3',
     'jlink-venus',
   ],
   env: {
     ZEPHYR_TOOLCHAIN_VARIANT: 'zephyr',
-    ZEPHYR_14_SDK_INSTALL_DIR: zephyrSDK_14.homeDir,
-    ZEPHYR_16_SDK_INSTALL_DIR: zephyrSDK_16.homeDir,
+    ZEPHYR_SDK_INSTALL_DIR: zephyrSDK.homeDir,
     GNUARMEMB_TOOLCHAIN_PATH: gcc.homeDir,
   },
 };
